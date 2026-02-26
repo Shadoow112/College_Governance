852aba7e278b1bceba185f134ab1b4be6668127e
Shadoow112 <118993940+Shadoow112@users.noreply.github.com>
2026-02-26 10:48:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const form = document.getElementById("form")
 
+form.addEventListener("submit",function(e){
+
+e.preventDefault()
+
+let nama = document.getElementById("nama").value
+let email = document.getElementById("email").value
+let hp = document.getElementById("hp").value
+
+let data = {
+nama:nama,
+email:email,
+hp:hp
+}
+
+let users = JSON.parse(localStorage.getItem("users")) || []
+
+users.push(data)
+
+localStorage.setItem("users",JSON.stringify(users))
+
+document.getElementById("msg").innerText="Registrasi berhasil"
+
+form.reset()
+
+})
